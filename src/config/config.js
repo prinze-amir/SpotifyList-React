d85b2c72d7ad459c9e1c94a6a6a018d542847f1e
Prinze Amir <myprinzeamir@gmail.com>
2023-12-27 14:36:48 -0500

    added github page url for redirect

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const clientId = process.env.REACT_APP_CLIENT_ID;
 const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
-const redirectURI = window.location.origin +'/';
+const redirectURI = window.location.origin +'/SpotifyList-React/';
 console.log(redirectURI)
 
 export {clientId, clientSecret, redirectURI}
