fd981560c0f6ce3c0c69a7721436fba539c5d8bf
Prinze Amir <myprinzeamir@gmail.com>
2023-12-27 01:52:06 -0500

    changed config

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 const clientId = process.env.REACT_APP_CLIENT_ID;
 const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
-let redirectURI
-const ENV = process.env;
-console.dir(ENV)
-if (ENV.NODE_ENV === 'development'){
-    redirectURI = 'http://localhost:3000/'
-} else {
-    redirectURI = 'https://prinze-amir.github.io/SpotifyList-React/'
-}
+let redirectURI = window.location.origin + '/';
+// const ENV = process.env;
+// console.dir(ENV)
+// if (ENV.NODE_ENV === 'development'){
+//     redirectURI = 'http://localhost:3000/'
+// } else {
+//     redirectURI = 'https://prinze-amir.github.io/SpotifyList-React/'
+// }
 console.log(redirectURI)
 
 export {clientId, clientSecret, redirectURI}
